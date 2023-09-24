f190fa5dcdac4cc02aeddb6f31238413a39bd2d6
theSiriusDebug <misha.ch2006@gmail.com>
2023-09-24 13:16:48 +0300

    add project structure.

---
+++ src/test/java/TextFeed/TextFeedApplicationTests.java
@@ -0,0 +1,13 @@
+package TextFeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextFeedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
