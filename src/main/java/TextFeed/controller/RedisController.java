a353cf51c1a1ef0c05351d1bc19de27121d1db62
theSiriusDebug <misha.ch2006@gmail.com>
2023-09-27 10:14:47 +0300

    add controller.

--- src/main/java/TextFeed/controller/RedisController.java
+++ src/main/java/TextFeed/controller/RedisController.java
@@ -1,42 +1,45 @@
 package TextFeed.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.Jedis;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
-@RestController
-@RequestMapping("/api/redis")
-public class RedisController {
-
-    @Autowired
-    private RedisTemplate<String, String> redisTemplate;
-
-    @GetMapping("/getAll")
-    public Map<String, String> getAllValues() {
-        Map<Object, Object> allEntries = redisTemplate.opsForHash().entries("myHash");
-        // Convert the keys and values to strings
-        Map<String, String> stringEntries = new HashMap<>();
-        for (Map.Entry<Object, Object> entry : allEntries.entrySet()) {
-            stringEntries.put(entry.getKey().toString(), entry.getValue().toString());
-        }
-        return stringEntries;
+@Controller
+class RedisController {
+    Jedis jedis = new Jedis("localhost", 6379);
+    public void add(String key, String value){
+        jedis.set(key, value);
+        jedis.close();
+    }
+    @GetMapping("/add/{value}")
+    @ResponseBody
+    public String add(@PathVariable String value){
+        System.out.println(jedis.dbSize());
+        add("text" + jedis.dbSize(), value);
+        System.out.println("");
+        System.out.println(jedis.dbSize());
+        System.out.println(value);
+        return "add";
     }
 
-    @PostMapping("/add")
-    public void addValue(@RequestBody String value) {
-        String key = generateKey(); // Генерируем ключ
-        redisTemplate.opsForHash().put("myHash", key, value); // Добавляем значение с сгенерированным ключом
-    }
+    @GetMapping("/")
+    @ResponseBody // Эта аннотация указывает, что метод вернет данные, а не будет искать шаблон
+    public String getAll(Model model) {
+        Set<String> keys = jedis.keys("text*");
+        List<String> values = new ArrayList<>();
 
-    private String generateKey() {
-        Map<Object, Object> allEntries = redisTemplate.opsForHash().entries("myHash");
-        int sum = 0;
-        for (Object value : allEntries.values()) {
-            sum += Integer.parseInt(value.toString());
+        for (String key : keys) {
+            String value = jedis.get(key);
+            values.add(value);
         }
-        return String.valueOf(sum);
+        model.addAttribute("values", values); // Передаем значения в модель
+        return "home";
     }
 }
