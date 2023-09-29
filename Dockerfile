082000913810b0b585e0992578a114e02c975814
theSiriusDebug <misha.ch2006@gmail.com>
2023-09-29 10:29:20 +0300

    edit docker files.

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM openjdk:21
 ADD /target/TextFeed-0.0.1-SNAPSHOT.jar backend.jar
+EXPOSE 8085
 ENTRYPOINT ["java", "-jar", "backend.jar"]
--- src/main/java/TextFeed/config/RedisConfig.java
+++ src/main/java/TextFeed/config/RedisConfig.java
@@ -1,20 +1,28 @@
 package TextFeed.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
+@EnableCaching
 public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private Integer port;
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName("localhost");
-        config.setPort(8080);
+        config.setHostName(host);
+        config.setPort(port);
         return new JedisConnectionFactory(config);
     }
 
