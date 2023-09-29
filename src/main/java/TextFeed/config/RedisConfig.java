24d01a9afb4ffec84ae2585bab07ebc58239938f
theSiriusDebug <misha.ch2006@gmail.com>
2023-09-29 09:15:17 +0300

    add docker files.

--- src/main/java/TextFeed/config/RedisConfig.java
+++ src/main/java/TextFeed/config/RedisConfig.java
@@ -3,16 +3,26 @@ package TextFeed.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
 @Configuration
 public class RedisConfig {
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName("localhost");
+        config.setPort(8080);
+        return new JedisConnectionFactory(config);
+    }
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-        jedisConnectionFactory.setHostName("localhost"); // Set your Redis server host
-        jedisConnectionFactory.setPort(6379); // Set your Redis server port
-        return jedisConnectionFactory;
+    public RedisTemplate<String, Object> redisTemplate(){
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        return redisTemplate;
     }
 }
